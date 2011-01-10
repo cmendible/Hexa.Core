146202aa8ffc4305d89c81f1ad34b8d49aadef90
Carlos Mendible <cmendible@gmail.com>
2011-01-10 14:46:39 +0000

    Assembly version set to 3.0.0.0

--- Hexa.Core.Tests/Properties/AssemblyInfo.cs
+++ Hexa.Core.Tests/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
