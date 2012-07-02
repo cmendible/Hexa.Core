7a5daeea5cee751c7d0d4db432b32fbd975003b5
Carlos Mendible <cmendible@gmail.com>
2012-07-02 22:57:29 +0200

    Fixed copyright header (StylCop rule: SA1005)

--- Hexa.Core/Domain/Specification/TrueSpecification.cs
+++ Hexa.Core/Domain/Specification/TrueSpecification.cs
@@ -1,14 +1,14 @@
-﻿//===================================================================================
+﻿// ===================================================================================
 // Microsoft Developer & Platform Evangelism
-//=================================================================================== 
+// =================================================================================== 
 // THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
 // EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
 // OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-//===================================================================================
+// ===================================================================================
 // Copyright (c) Microsoft Corporation.  All Rights Reserved.
 // This code is released under the terms of the MS-LPL license, 
 // http://microsoftnlayerapp.codeplex.com/license
-//===================================================================================
+// ===================================================================================
 using System;
 using System.Linq.Expressions;
 
