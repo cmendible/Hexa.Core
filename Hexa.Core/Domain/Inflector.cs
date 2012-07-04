e98042766e12f3bb54c41c26fd8c72dc66b4fae5
Carlos Mendible <cmendible@gmail.com>
2012-07-04 18:39:06 +0200

    code styling...

--- Hexa.Core/Domain/Inflector.cs
+++ Hexa.Core/Domain/Inflector.cs
@@ -1,6 +1,6 @@
 ﻿#region Header
 
-//Copyright 2010 Ritesh Rao
+// Copyright 2010 Ritesh Rao
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
