4cf1d29dcb5b85e8308c905a4dbe19ea977c2bdd
cmendible <carlos.mendible@hexasystems.com>
2012-07-17 15:36:03 +0200

    Enhancing the build experience...

--- BuildAndTest.bat
+++ BuildAndTest.bat
@@ -4,5 +4,5 @@ if exist "%programfiles%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"	call "%p
 if exist "%programfiles(x86)%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" call "%programfiles(x86)%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 if exist "%programfiles%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"	call "%programfiles%\Microsoft Visual Studio 11	.0\VC\vcvarsall.bat" x86
 echo Building solution...
-call msbuild.exe BuildAndTest.msbuild
+call msbuild.exe BuildAndTest.msbuild /p:"EnableTests=true"
 pause
