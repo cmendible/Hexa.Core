b05a6abc511f5f1ac298df4968d797baef2bee42
cmendible <carlos.mendible@hexasystems.com>
2012-10-18 17:32:14 +0200

    Added EnumerableModel so we can consume IEnumerables in awesome multilookup escenarios....

---
+++ Hexa.Core.Web.Mvc/EnumerableModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Hexa.Core.Web.Mvc.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class EnumerableModel<T>
+    {
+        public EnumerableModel(IEnumerable<T> list)
+        {
+            this.Items = list;
+        }
+
+        public IEnumerable<T> Items { get; set; }
+    }
+}
