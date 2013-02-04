7032bb105b1425a91fe51ca9ce948cf8327cb324
cmendible <carlos.mendible@hexasystems.com>
2013-02-04 16:50:36 +0100

    Added Hexa.Core.nuspec Added Hexa.Core.NHibernate.nuspec Added Hexa.Core.Web.Mvc.nuspec Added BuildNuGetPackages.bat Ignored .nupkg files from repo.

---
+++ BuildNuGetPackages.bat
@@ -0,0 +1,4 @@
+call ".nuget/NuGet.exe" pack .nuget/Hexa.Core.nuspec -o ".nuget/"
+call ".nuget/NuGet.exe" pack .nuget/Hexa.Core.NHibernate.nuspec -o ".nuget/"
+call ".nuget/NuGet.exe" pack .nuget/Hexa.Core.Web.Mvc.nuspec -o ".nuget/"
+pause
