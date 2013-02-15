0162cc8c14496482f212e8ee88a6ed38762597e2
cmendible <carlos.mendible@hexasystems.com>
2013-02-15 11:40:12 +0100

    Hexa.Core.Web.Mvc project now uses Nuget to get the MVC binaries & references... Added CustomAuthorizeAttribute.cs Added AuthorizeActionLinkExtensions.cs

---
+++ Hexa.Core.Web.Mvc/Security/CustomAuthorizeAttribute.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CustomAuthorizeAttribute.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Hexa.Core.Web.Mvc.Security
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Web.Mvc;
+    using Hexa.Core.Web.Security;
+
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        #region Methods
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+
+            // Try Get IAuthorizationService 
+            IAuthorizationService authorizationService = ServiceLocator.TryGetInstance<IAuthorizationService>();
+            if (authorizationService != null)
+            {
+                // Get Action to Authorize
+                string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerType.FullName;
+                string actionName = filterContext.ActionDescriptor.ActionName;
+                string ip = filterContext.HttpContext.Request.UserHostAddress;
+
+                string actionToAuthorize = string.Format("{0}.{1}", controllerName, actionName);
+                // If user is authorized return
+                if (authorizationService.IsAuthorized(actionToAuthorize))
+                {
+                    return;
+                }
+            }
+            else
+            {
+                // No IAuthorizationService is in place so return;
+                return;
+            }
+
+            // The user is not allowed to execute the Action. An Unauthorized result is raised.
+            filterContext.Result = new HttpUnauthorizedResult();
+        }
+
+        #endregion Methods
+    }
+}
---
+++ Hexa.Core.Web.Mvc/Security/Extensions/AuthorizeActionLinkExtensions.cs
@@ -0,0 +1,93 @@
+﻿
+namespace Hexa.Core.Web.Mvc.Extensions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Web.Mvc;
+    using System.Web.Mvc.Html;
+    using System.Web.Routing;
+    using System.Globalization;
+
+    public static class AuthorizeActionLinkExtensions
+    {
+        public static MvcHtmlString AuthorizeActionLink(this HtmlHelper helper, string linkText, string actionName, string controllerName, object routeValues, object htmlAttributes)
+        {
+            if (HasActionPermission(helper, actionName, controllerName))
+                return helper.ActionLink(linkText, actionName, controllerName, routeValues, htmlAttributes);
+
+            return MvcHtmlString.Empty;
+        }
+
+        public static MvcHtmlString AuthorizeActionLink(this HtmlHelper helper, string linkText, string actionName, string controllerName)
+        {
+            if (HasActionPermission(helper, actionName, controllerName))
+                return helper.ActionLink(linkText, actionName, controllerName);
+
+            return MvcHtmlString.Empty;
+        }
+
+        public static MvcHtmlString AuthorizeActionLink(this HtmlHelper helper, string linkText, string actionName, string controllerName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
+        {
+            if (HasActionPermission(helper, actionName, controllerName))
+
+                return helper.ActionLink(linkText, actionName, controllerName, routeValues, htmlAttributes);
+
+            return MvcHtmlString.Empty;
+        }
+
+        private static bool HasActionPermission(this HtmlHelper htmlHelper, string actionName, string controllerName)
+        {
+            ControllerBase controllerToLinkTo = string.IsNullOrEmpty(controllerName)
+                ? htmlHelper.ViewContext.Controller
+                : GetControllerByName(htmlHelper, controllerName);
+
+            ControllerContext controllerContext = new ControllerContext(htmlHelper.ViewContext.RequestContext, controllerToLinkTo);
+
+            ReflectedControllerDescriptor controllerDescriptor = new ReflectedControllerDescriptor(controllerToLinkTo.GetType());
+            ActionDescriptor actionDescriptor = controllerDescriptor.FindAction(controllerContext, actionName);
+
+            return ActionIsAuthorized(controllerContext, actionDescriptor);
+        }
+
+        private static bool ActionIsAuthorized(ControllerContext controllerContext, ActionDescriptor actionDescriptor)
+        {
+            if (actionDescriptor == null)
+                return false;
+
+            AuthorizationContext authContext = new AuthorizationContext(controllerContext, actionDescriptor);
+            foreach (Filter authFilter in FilterProviders.Providers.GetFilters(authContext, actionDescriptor))
+            {
+                if (authFilter.Instance is System.Web.Mvc.AuthorizeAttribute)
+                {
+                    ((IAuthorizationFilter)authFilter.Instance).OnAuthorization(authContext);
+
+                    if (authContext.Result != null)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static ControllerBase GetControllerByName(HtmlHelper helper, string controllerName)
+        {
+            IControllerFactory factory = ControllerBuilder.Current.GetControllerFactory();
+
+            IController controller = factory.CreateController(helper.ViewContext.RequestContext, controllerName);
+
+            if (controller == null)
+            {
+                throw new System.InvalidOperationException(
+                    string.Format(
+                        CultureInfo.CurrentUICulture,
+                        "Controller factory {0} controller {1} returned null",
+                        factory.GetType(),
+                        controllerName));
+            }
+
+            return (ControllerBase)controller;
+        }
+
+    }
+}
