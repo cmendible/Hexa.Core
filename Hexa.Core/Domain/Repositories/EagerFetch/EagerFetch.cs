3ceae7881a8a5e19c2c751c684808eadb46a75d4
cmendible <carlos.mendible@hexasystems.com>
2013-03-12 09:53:47 +0100

    Fixed folder structure

---
+++ Hexa.Core/Domain/Repositories/EagerFetch/EagerFetch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Hexa.Core.Domain
+{
+    public static class EagerFetch
+    {
+        public static Func<IFetchProvider> FetchingProvider;
+
+        public static IFetchRequest<TOriginating, TRelated> Fetch<TOriginating, TRelated>(
+            this IQueryable<TOriginating> query, Expression<Func<TOriginating, TRelated>> relatedObjectSelector)
+            where TOriginating : class
+        {
+            return FetchingProvider().Fetch(query, relatedObjectSelector);
+        }
+
+        public static IFetchRequest<TOriginating, TRelated> FetchMany<TOriginating, TRelated>(
+            this IQueryable<TOriginating> query,
+            Expression<Func<TOriginating, IEnumerable<TRelated>>> relatedObjectSelector)
+            where TOriginating : class
+        {
+            return FetchingProvider().FetchMany(query, relatedObjectSelector);
+        }
+
+        public static IFetchRequest<TQueried, TRelated> ThenFetch<TQueried, TFetch, TRelated>(
+            this IFetchRequest<TQueried, TFetch> query, Expression<Func<TFetch, TRelated>> relatedObjectSelector)
+            where TQueried : class
+        {
+            return FetchingProvider().ThenFetch(query, relatedObjectSelector);
+        }
+
+        public static IFetchRequest<TQueried, TRelated> ThenFetchMany<TQueried, TFetch, TRelated>(
+            this IFetchRequest<TQueried, TFetch> query,
+            Expression<Func<TFetch, IEnumerable<TRelated>>> relatedObjectSelector)
+            where TQueried : class
+        {
+            return FetchingProvider().ThenFetchMany(query, relatedObjectSelector);
+        }
+    }
+}
--- hexa.core/Domain/Repositories/EagerFetch/IFetchRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Linq;
-
-namespace Hexa.Core.Domain
-{
-    public interface IFetchRequest<TQueried, TFetch> : IOrderedQueryable<TQueried>
-    {
-    }
-}
