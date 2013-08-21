f6d63e7ec9eaf5844192db529703e5c0e2cfa978
cmendible <carlos.mendible@hexasystems.com>
2013-08-21 12:17:38 +0200

    Added header to file

--- Hexa.Core.Web.Mvc/Grid/GridData.cs
+++ Hexa.Core.Web.Mvc/Grid/GridData.cs
@@ -1,4 +1,9 @@
-﻿namespace Hexa.Core.Web.Mvc.Helpers
+﻿//----------------------------------------------------------------------------------------------
+// <copyright file="GridData.cs" company="HexaSystems Inc">
+// Copyright (c) HexaSystems Inc. Licensed under the Apache License, Version 2.0 (the "License")
+// </copyright>
+//-----------------------------------------------------------------------------------------------
+namespace Hexa.Core.Web.Mvc.Helpers
 {
     using System;
     using System.Collections.Generic;
