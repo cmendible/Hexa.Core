31c69a9997844cf2a796f46eb0d94f082cb2d9d1
Carlos Mendible <Carlos Mendible>
2017-03-15 23:19:34 +0100

    RequireDbContextAttribute

---
+++ src/Hexa.Core.EF/Mvc/RequireDbContextAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace Hexa.Core.Web.Mvc
+{
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.EntityFrameworkCore;
+
+    public class RequireDbContextAttribute : ActionFilterAttribute
+    {
+        private readonly DbContext context;
+
+        public RequireDbContextAttribute(DbContext context)
+        {
+            this.context = context;
+        }
+        
+        public override void OnActionExecuted(ActionExecutedContext actionExecutedContext)
+        {
+            try
+            {
+                this.context?.SaveChanges();
+            }
+            finally
+            {
+                this.context?.Dispose();
+            }
+        }
+    }
+}
